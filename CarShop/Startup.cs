7953260d1ac5182068f390ef12138930ecbd264f
George <gs.ivanov@abv.bg>
2024-03-16 08:37:28 +0200

    Add project files.

---
+++ CarShop/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace CarShop
+{
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using System.Threading.Tasks;
+
+    class Startup
+    {
+        public static async Task Main()
+         => await HttpServer
+          .WithRoutes(routes => routes
+              .MapStaticFiles()
+              .MapControllers())
+          .WithServices(services => services
+              .Add<IViewEngine, CompilationViewEngine>())
+          .Start();
+    }
+}
